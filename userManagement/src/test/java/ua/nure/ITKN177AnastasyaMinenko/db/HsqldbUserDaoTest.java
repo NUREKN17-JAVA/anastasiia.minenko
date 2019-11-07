b4f76fa5daf31820c9ca11ba546fbe7ea2455348
MinenkoNastya <anastasiia.minenko@nure.ua>
2019-11-07 16:34:23 +0200

    step2

---
+++ userManagement/src/test/java/ua/nure/ITKN177AnastasyaMinenko/db/HsqldbUserDaoTest.java
@@ -0,0 +1,63 @@
+package ua.nure.ITKN177AnastasyaMinenko.db;
+
+import java.util.Calendar;
+
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+
+import ua.nure.ITKN177AnastasyaMinenko.db.DatabaseException;
+import ua.nure.ITKN177AnastasyaMinenko.db.HsqldbUserDao;
+import ua.nure.ITKN177AnastasyaMinenko.User;
+
+public class HsqldbUserDaoTest extends DatabaseTestCase {
+	private HsqldbUserDao dao;
+
+	private static final String LAST_NAME = "Cena";
+	private static final String FIRST_NAME = "John";
+	private static final int YEAR = 2010;
+	private static final int MONTH = 1;
+	private static final int CREATE_DAY = 1;
+
+
+
+	public void testCreate() throws DatabaseException {
+		User user = new User();
+
+		user.setFirstName(FIRST_NAME);
+		user.setLastName(LAST_NAME);
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(YEAR, MONTH, CREATE_DAY);
+		user.setDateofBirth(calendar.getTime());
+		assertNull(user.getId());
+		User userToCheck = dao.create(user);
+		assertNotNull(userToCheck);
+		assertNotNull(userToCheck.getId());
+		assertEquals(user.getFirstName(), userToCheck.getFirstName());
+        assertEquals(user.getLastName(), userToCheck.getLastName());
+        assertEquals(user.getDateofBirth(), userToCheck.getDateofBirth());
+
+	}
+
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Override
+	protected IDatabaseConnection getConnection() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
