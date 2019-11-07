bf3e3c6b7ba8509f7717edf9c3f394b02f3afc00
MinenkoNastya <anastasiia.minenko@nure.ua>
2019-11-07 16:10:31 +0200

    step1

---
+++ userManagement/src/main/java/ua/nure/ITKN177AnastasyaMinenko/db/Dao.java
@@ -0,0 +1,17 @@
+package ua.nure.ITKN177AnastasyaMinenko.db;
+
+import java.util.Collection;
+
+import ua.nure.ITKN177AnastasyaMinenko.db.DatabaseException;
+
+public interface Dao<T> {
+	T create(T entity) throws DatabaseException;
+
+	void update(T entity) throws DatabaseException;
+
+	void delete(T entity) throws DatabaseException;
+
+	T find(long id) throws DatabaseException;
+
+	Collection<T> findAll() throws DatabaseException;
+}
---
+++ userManagement/src/main/java/ua/nure/ITKN177AnastasyaMinenko/db/HsqldbUserDao.java
@@ -0,0 +1,40 @@
+package ua.nure.ITKN177AnastasyaMinenko.db;
+
+import java.util.Collection;
+
+import ua.nure.ITKN179.MaslovskyiMykyta.User;
+import ua.nure.ITKN179.MaslovskyiMykyta.db.Dao;
+
+public class HsqldbUserDao implements Dao<User> {
+
+	@Override
+	public User create(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User find(long id) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<User> findAll() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
