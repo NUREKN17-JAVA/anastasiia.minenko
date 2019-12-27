b9bd44510dcd69c94a1e3cfd7ec860b212dcf9d6
MinenkoNastya <anastasiia.minenko@nure.ua>
2019-12-27 21:57:38 +0200

    agent

---
+++ usermanagement/usermanagement/src/test/java/ua/nure/ITKN177/AnastasiyaMinenko/domain/db/DaoFactoryTest.java
@@ -0,0 +1,24 @@
+package ua.nure.ITKN177.AnastasiyaMinenko.domain.db;
+
+import junit.framework.TestCase;
+import ua.nure.ITKN177.AnastasiyaMinenko.domain.db.DaoFactory;
+import ua.nure.ITKN177.AnastasiyaMinenko.domain.db.UserDao;
+
+public class DaoFactoryTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testGetUserDao() {
+		try {
+			DaoFactory daoFactory = DaoFactory.getInstance();
+			assertNotNull("DaoFactory instance is null", daoFactory);
+			UserDao userDao = daoFactory.getUserDao();
+			assertNotNull("UserDao instance is null", userDao);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+}
